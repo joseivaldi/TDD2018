61f3e1477860b343f47c037ad5028a1d67b1bcb1
joseivaldi <40646985+joseivaldi@users.noreply.github.com>
2018-09-18 17:11:50 -0300

    Declarando funciones

--- src/Mazo.php
+++ src/Mazo.php
@@ -7,4 +7,20 @@ class Mazo {
   public function mezclar() {
     return TRUE;
   }
+  
+  public function getTop() {
+    return TRUE;
+  }
+  
+  public function cortar() {
+    return TRUE;
+  }
+  
+  public function cantidad() {
+    return TRUE;
+  }
+  
+  public function nuevacarta() {
+    return TRUE;
+  }
 }
