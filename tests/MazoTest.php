44ab85ef190b60186f4469b4744b0dd028b20da7
Ignacioe <mypromega@hotmail.com>
2018-11-21 15:43:01 -0300

    cant al constructor
    Paso 0 como cantidad de cartas cuando declaro mazos nuevos

--- tests/MazoTest.php
+++ tests/MazoTest.php
@@ -10,37 +10,37 @@ class MazoTest extends TestCase {
      * Valida que se puedan crear mazos de cartas.
      */
     public function testExiste() {
-        $mazo = new Mazo();
+        $mazo = new Mazo(0);
         $this->assertTrue(isset($mazo));
     }
 
     public function testMezclable() {
-        $mazo = new Mazo();
+        $mazo = new Mazo(0);
         $this->assertTrue($mazo->mezclar());
     }
 
     public function testObtenerCarta() {
-        $mazo = new Mazo();
+        $mazo = new Mazo(0);
         $this->assertTrue($mazo->getTop());
     }
 
     public function testCortar() {
-        $mazo = new Mazo();
+        $mazo = new Mazo(0);
         $this->assertTrue($mazo->cortar());
     }
     
     public function testCantidadCartas() {
-        $mazo = new Mazo();
+        $mazo = new Mazo(0);
         $this->assertEquals(0,$mazo->cantidad());
     }
   
     public function testAgregarCarta() {
-        $mazo = new Mazo();
+        $mazo = new Mazo(0);
         $this->assertTrue($mazo->nuevacarta());
     }  
     
     public function testEsVacio() {
-        $mazo = new Mazo();
+        $mazo = new Mazo(0);
         $this->assertTrue($mazo->esvacio());
     }  
 }
