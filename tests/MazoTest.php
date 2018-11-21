525224ae314f6ca987dfcee8502e4f596a3817ba
Ignacioe <mypromega@hotmail.com>
2018-11-21 16:52:34 -0300

    correcion en llamadas a tests

--- tests/MazoTest.php
+++ tests/MazoTest.php
@@ -23,8 +23,8 @@ class MazoTest extends TestCase {
 
     public function testObtenerCarta() {
         $mazo = new Mazo([0,10]);
-        $this->assertEquals($mazo->testObtenerCarta(0),0);
-        $this->assertEquals($mazo->testObtenerCarta(1),10);
+        $this->assertEquals($mazo->obtenerCarta(0),0);
+        $this->assertEquals($mazo->obtenerCarta(1),10);
     }
 
     public function testCortar() {
@@ -37,8 +37,8 @@ class MazoTest extends TestCase {
         $this->assertEquals(0,$mazo->cantidad());
         $this->assertNotEquals(20,$mazo->cantidad());
         $mazo2 = new Mazo ([0]);
-        $this->assertEquals(1,$mazo->cantidad());
-        $this->assertNotEquals(20,$mazo->cantidad());
+        $this->assertEquals(1,$mazo2->cantidad());
+        $this->assertNotEquals(20,$mazo2->cantidad());
     }
   
     public function testAgregarCarta() {
