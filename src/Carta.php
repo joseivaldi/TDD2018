9d3c34f38ef46434a7fd05bc68355bd84084f8db
Ignacioe <mypromega@hotmail.com>
2018-11-24 20:51:16 -0300

    Agrego construct
    Con muuuuchos ifs

--- src/Carta.php
+++ src/Carta.php
@@ -3,4 +3,31 @@
 namespace TDD;
 
 class Carta {
+    protected $numero;
+    protected $palo;
+    protected $pokeroespañol;
+
+    public function __construct($num, $pal, $tipo){
+        if($tipo=="Poker"){
+            if($pal=="Diamantes"||$pal=="Picas"||$pal=="Corazones"||$pal=="Treboles"){
+                if($num=='A'||$num=='J'||$num=='Q'||$num=='K'||($num>1&&$num<11)){
+                    $this->numero=$num;
+                    $this->palo=$pal;
+                    $this->pokeroespañol=$tipo;
+                }else return FALSE
+            }else return FALSE
+        }
+        elseif($tipo=="Española"){
+            if($pal=="Oro"||$pal=="Basto"||$pal=="Espada"||$pal=="Copa"){
+                if($num>=1&&$num<=12){
+                    $this->numero=$num;
+                    $this->palo=$pal;
+                    $this->pokeroespañol=$tipo;
+                }else return FALSE
+            }else return FALSE
+        }else return FALSE
+    }
+
+
+    
 }
