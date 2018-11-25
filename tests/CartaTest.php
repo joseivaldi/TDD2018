54d6a8e62a5471f12de7612adff4b612c25e2b9d
Ignacioe <mypromega@hotmail.com>
2018-11-24 22:23:12 -0300

    borro caso de test

--- tests/CartaTest.php
+++ tests/CartaTest.php
@@ -8,8 +8,6 @@ class CartaTest extends TestCase {
    
     public function testCartaValida(){
         $unooro=new Carta('1',"Oro", "Española");
-        $dosdiamantes=new Carta('2',"Diamantes", "Poker");
         $this->assertTrue(esCarta($unooro));
-        $this->assertFalse(esCarta($dosdiamantes));
     }
 }
