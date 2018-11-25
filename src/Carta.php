20611aa4b7d2acc1180fd377f7c251f01736ca80
Ignacioe <mypromega@hotmail.com>
2018-11-24 21:11:22 -0300

    muevo construct a esCarta y agrego test

--- src/Carta.php
+++ src/Carta.php
@@ -3,31 +3,30 @@
 namespace TDD;
 
 class Carta {
-    protected $numero;
+    protected $num;
     protected $palo;
-    protected $pokeroespañol;
+    protected $tip;
 
-    public function __construct($num, $pal, $tipo){
-        if($tipo=="Poker"){
-            if($pal=="Diamantes"||$pal=="Picas"||$pal=="Corazones"||$pal=="Treboles"){
-                if($num=='A'||$num=='J'||$num=='Q'||$num=='K'||($num>1&&$num<11)){
-                    $this->numero=$num;
-                    $this->palo=$pal;
-                    $this->pokeroespañol=$tipo;
-                }else return FALSE
-            }else return FALSE
-        }
-        elseif($tipo=="Española"){
-            if($pal=="Oro"||$pal=="Basto"||$pal=="Espada"||$pal=="Copa"){
-                if($num>=1&&$num<=12){
-                    $this->numero=$num;
-                    $this->palo=$pal;
-                    $this->pokeroespañol=$tipo;
-                }else return FALSE
-            }else return FALSE
-        }else return FALSE
+    public function __construct($numero, $pal, $tipo){
+        $this->$num=$numero;
+        $this->$palo=$pal;
+        $this->$tip=$tipo;
     }
 
-
-    
+    public function esCarta($carta){
+        if($carta->tip=="Poker"){
+            if($carta->palo=="Diamantes"||$carta->palo=="Picas"||$carta->palo=="Corazones"||$carta->palo=="Treboles"){
+                if($carta->num=='A'||$carta->num=='J'||$carta->num=='Q'||$carta->num=='K'||($carta->num>1&&$carta->num<11)){
+                    return TRUE;
+                }else return FALSE;
+            }else return FALSE;
+        }
+        elseif($carta->tip=="Española"){
+            if($carta->palo=="Oro"||$carta->palo=="Basto"||$carta->palo=="Espada"||$carta->palo=="Copa"){
+                if($carta->num>=1&&$carta->num<=12){
+                    return TRUE;
+                }else return FALSE;
+            }else return FALSE;
+        }else return FALSE;
+    }
 }
--- tests/CartaTest.php
+++ tests/CartaTest.php
@@ -5,5 +5,11 @@ namespace TDD;
 use PHPUnit\Framework\TestCase;
 
 class CartaTest extends TestCase {
-
+   
+    public function testCartaValida(){
+        $unooro=new Carta('1',"Oro", "Española");
+        $unodiamantes=new Carta('1',"Diamantes", "Poker");
+        $this->assertTrue(esCarta($unooro));
+        $this->assertFalse(esCarta($unodiamantes));
+    }
 }
