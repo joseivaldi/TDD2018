1295aade13c77de6e002fd95d18e920e29e92d13
Ignacioe <mypromega@hotmail.com>
2018-11-24 22:46:21 -0300

    string

--- tests/CartaTest.php
+++ tests/CartaTest.php
@@ -7,7 +7,7 @@ use PHPUnit\Framework\TestCase;
 class CartaTest extends TestCase {
    
     public function testCartaValida(){
-        $unooro=new Carta('1',"Oro", "Española");
+        $unooro=new Carta("1","Oro", "Española");
         $this->assertTrue(esCarta($unooro));
     }
 }
